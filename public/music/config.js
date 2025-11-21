43b4f480771b840ba302ef3ad873fa6b4fd76a2e
mcyzsx <mcyzsx@outlook.com>
2025-11-21 20:53:55 +0800

    添加音乐馆页面测试

---
+++ public/music/config.js
@@ -0,0 +1,21 @@
+var userId = "13681647281";
+var userServer = "netease";
+var userType = "playlist";
+
+window.meting_api = 'https://meting.kemiaosw.top/api?server=:server&type=:type&id=:id&r=:r';
+// var localMusic = [{
+//     name: '重生之我在异乡为异客',
+//     artist: '王睿卓',
+//     url: '/music/重生之我在异乡为异客.mp3',
+//     cover: '/music/重生之我在异乡为异客.png',
+//     lrc: '/music/重生之我在异乡为异客.lrc'
+// },
+// {
+//     name: '落',
+//     artist: '唐伯虎',
+//     url: '/music/落.mp3',
+//     cover: '/music/落.png',
+//     lrc: '/music/落.lrc'
+// }
+// ];
+// var remoteMusic = "./musiclist.json"
