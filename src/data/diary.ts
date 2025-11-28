877665baebb552de360f303431d1d085919416ec
mcyzsx <mcyzsx@outlook.com>
2025-11-28 22:34:44 +0800

    添加stean页面和完善一些设置

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -37,6 +37,13 @@ export interface DiaryEntry {
 /* ---------- 日记数据 ---------- */
 export const diaryEntries: DiaryEntry[] = [
   {
+    text: "今天算是完成了ryuchan博客的魔改吧。",
+    date: "2025-11-28 22:18",
+    tags: ["日常"],
+    location: "南京",
+    url: ["https://boke.zsx815.top/steam"],
+  },
+  {
     text: "今天算是完成了实训的第一部分，然后搞了icefox的v3.0版本。",
     date: "2025-11-26 22:51",
     tags: ["日常"],
