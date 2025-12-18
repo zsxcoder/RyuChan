91990737e8deafb78e552df4e9cb1283937a9943
mcyzsx <mcyzsx@outlook.com>
2025-12-18 23:25:24 +0800

    添加日记和导航网站

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -38,6 +38,14 @@ export interface DiaryEntry {
 export const diaryEntries: DiaryEntry[] = [
   {
     text: "今天给博客把友链检测页面放到了友链页。然后搞了个中转页",
+    date: "2025-12-18 23:16",
+    tags: ["日常"],
+    location: "南京",
+    github: ["https://github.com/miantiao-me/BroadcastChannel", "https://github.com/wallleap/ethereal"],
+    url: ["httpd://tg.mcyzsx.top", "httpd://myblog.mcyzsx.top"],
+  },
+  {
+    text: "今天给博客把友链检测页面放到了友链页。然后搞了个中转页",
     date: "2025-12-14 14:32",
     tags: ["日常"],
     location: "南京",
